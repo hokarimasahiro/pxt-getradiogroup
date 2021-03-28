32f7410a7cfd769fb562a6dfe32673ffb7965a90
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2021-03-29 05:34:27 +0900

    更新 getRadioGroup.ts

--- getRadioGroup.ts
+++ getRadioGroup.ts
@@ -29,7 +29,7 @@ namespace getradiogroup {
             radio.sendString("CQ," + control.deviceName())
             toTime = input.runningTime() + Math.randomRange(1000, 1050);
         }
-        if (rData != "" && radio.receivedPacket(RadioPacketProperty.SignalStrength) > -70) {
+        if (rData != "" && radio.receivedPacket(RadioPacketProperty.SignalStrength) > -50) {
             let rStrings = rData.split(",")
             if (rStrings[0] == "CQ") {
                 sGroup = Math.randomRange(10, 99);
@@ -40,6 +40,8 @@ namespace getradiogroup {
                 radioGroup = rGroup;
                 radio.setGroup(radioGroup)
             }
+        } else{
+            basic.pause(100);
         }
         return (radioGroup)
     }
